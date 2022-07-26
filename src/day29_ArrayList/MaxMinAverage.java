aa23ff0f82aae7c7d729804ffd25e08ae8a616a4
ozergungor <ozergungor2019@gmail.com>
2022-07-26 19:53:35 -0400

    ArrayList practice

--- src/day29_ArrayList/MaxMinAverage.java
+++ src/day29_ArrayList/MaxMinAverage.java
@@ -1,8 +1,38 @@
 package day29_ArrayList;
 
+import java.util.ArrayList;
+
 public class MaxMinAverage {
 
     public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+
+        System.out.print(list);
+
+        int max = list.get(0);
+
+        for (Integer each : list) {
+
+            if(each > max){
+                max = each;
+            }
+        }
+
+        System.out.println("\n max = " + max);
+
+
+
 
     }
 }
