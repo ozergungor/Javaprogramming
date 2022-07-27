3878317be4ad63f4aa88c8f0f0e576f6f922ff20
ozergungor <ozergungor2019@gmail.com>
2022-07-26 22:37:46 -0400

    ArrayList practice

---
+++ src/day29_ArrayList/RemoveIfMethod.java
@@ -0,0 +1,21 @@
+package day29_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveIfMethod {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        list.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        System.out.println(list);
+
+        list.removeIf(p-> p<5);
+        System.out.println(list);
+    }
+}
