386ec63139bd16a48ad1ab89327070a398c45e2b
ozergungor <ozergungor2019@gmail.com>
2022-08-14 21:12:06 -0400

    Day 43 Abstract Tesla

--- src/day43_Abstraction/Car/Tesla.java
+++ src/day43_Abstraction/Car/Tesla.java
@@ -1,7 +1,14 @@
 package day43_Abstraction.Car;
 
-public class Tesla {
+public final class Tesla extends Car {
+    public Tesla(String model, String color, int year, double price) {
+        super("Tesla", model, color, year, price);
+    }
+    public void start() {
 
-    
-
+        System.out.println("Say \"Start\"");
+    }
+    public final void autopilot(){
+        System.out.println(getBrand()+ " " + getModel() + " has auto pilot feature");
+    }
 }
