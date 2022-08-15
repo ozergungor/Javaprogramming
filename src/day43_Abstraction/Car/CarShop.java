6293c2b891f6bbc57ca231ce6020a31c9a2bff28
ozergungor <ozergungor2019@gmail.com>
2022-08-14 21:41:55 -0400

    Day 43 Abstract Honda

---
+++ src/day43_Abstraction/Car/CarShop.java
@@ -0,0 +1,9 @@
+package day43_Abstraction.Car;
+
+public class CarShop {
+
+    public static void main(String[] args) {
+
+        Honda honda = new Honda();
+    }
+}
--- src/day43_Abstraction/Car/Honda.java
+++ src/day43_Abstraction/Car/Honda.java
@@ -2,8 +2,8 @@ package day43_Abstraction.Car;
 
 public final class Honda extends Car {
 
-    public Honda(String brand, String model, String color, int year, double price) {
-        super(brand, model, color, year, price);
+    public Honda(String model, String color, int year, double price) {
+        super("Honda" model, color, year, price);
     }
     public void start(){
         System.out.println("Twist the key to ignition");
